6dc56da287de36383d739ce4a1245712552430ca
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2024-11-29 23:03:43 +0900

    Use SSL and the Puma web server in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,5 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert.alert-success' 
   end
 end
