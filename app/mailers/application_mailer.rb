1295f532f854cb82fd34436eaefcc8e09b125895
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2024-12-06 18:28:56 +0900

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "haruouma0528@docomo.ne.jp"
   layout "mailer"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,11 +4,13 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
+  # parallelize(workers: :number_of_processors, with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
+
+  # parallelize(workers: 1)
 
   # Add more helper methods to be used by all tests here...
   
