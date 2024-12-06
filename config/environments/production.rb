82302fba103da7f63a2f7e8a8723626e8381556d
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2024-12-06 18:42:13 +0900

    render_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://<あなたのRenderアプリ名>.onrender.com'
+  host = 'https://rails-sample-app.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
